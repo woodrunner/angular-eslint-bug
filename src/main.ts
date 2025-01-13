09a0d1d6f33f784b7febd538db0bd05bab75acc3
Kristof <verslegers.kristof@gmail.com>
2025-01-13 10:41:02 +0100

    added patch to remove names + triggered eslint warning for takeUntil

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,33 @@
 import 'zone.js';
 import {Component} from '@angular/core';
 import {bootstrapApplication} from '@angular/platform-browser';
-import { of, map } from 'rxjs';
+import {of, map} from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
-  `,
+    selector: 'app-root',
+    standalone: true,
+    template: `
+        <h1>Hello from {{ name }}!</h1>
+        <a target="_blank" href="https://angular.dev/overview">
+            Learn more about Angular
+        </a>
+    `,
 })
 export class App {
-  name = 'Angular';
+    name = 'Angular';
 
-  constructor() {
-    of(1, 2, 3)
-      .pipe(map(x => x * 2))
-      .subscribe({
-        next: value => console.log('Mapped value:', value),
-        complete: () => console.log('RxJS Completed'),
-      });
-  }
+    constructor() {
+        of(1, 2, 3)
+            .pipe(
+                takeUntil(this as any),
+                map(x => x * 2)
+            )
+            .subscribe({
+                next: value => console.log('Mapped value:', value),
+                complete: () => console.log('RxJS Completed'),
+            });
+    }
 }
 
 bootstrapApplication(App);
