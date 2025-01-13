bc511d80ac993530f12e3b4700ce0cdce9519765
woodrunner <verslegers.kristof@gmail.com>
2025-01-13 08:37:49 +0100

    fixed dependencies

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -2,15 +2,7 @@ const { FlatCompat } = require('@eslint/eslintrc');
 const js = require('@eslint/js');
 const join = require('path').join;
 const util = require('util');
-const nxEslintPlugin = require('@nx/eslint-plugin');
-const eslintPluginCypress = require('eslint-plugin-cypress');
-const eslintPluginImport = require('eslint-plugin-import');
-const eslintPluginJest = require('eslint-plugin-jest');
 const rxjs = require('@smarttools/eslint-plugin-rxjs');
-const eslintPluginRxjsAngular = require('eslint-plugin-rxjs-angular-updated');
-const eslintPluginSortClassMembers = require('eslint-plugin-sort-class-members');
-const eslintPluginTypescript = require('@typescript-eslint/eslint-plugin');
-const stylisticJs = require('@stylistic/eslint-plugin-js');
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
